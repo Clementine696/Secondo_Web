102bc247c7bd81a5dcc721bdbefbf789ae14147b
TtB00 <88588878+TtB00@users.noreply.github.com>
2024-02-07 22:54:37 +0700

    .

--- Front-Secondo/src/components/UI/CategoryCard/index.jsx
+++ Front-Secondo/src/components/UI/CategoryCard/index.jsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 function CateCard() {
   return (
     <div className="frame-cate-card">
-      <img
-        className="img-category"
-        src="https://www.pngmart.com/files/1/Sneaker-PNG-Transparent-Image.png"
-      />
-      <div className="text-category">
-        ประเภทของสินค้า
-      </div>
+      <Link style={{ textDecoration: "none" }} className="link-img-category">
+        <img
+          className="img-category"
+          src="https://www.pngmart.com/files/1/Sneaker-PNG-Transparent-Image.png"
+        />
+      </Link>
+      <Link style={{ textDecoration: "none" }} className="text-category">ประเภทของสินค้า</Link>
     </div>
   );
 }
