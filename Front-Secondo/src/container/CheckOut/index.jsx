8d1e161cc18a4e3ad882c21fbc7db54fe6c7f94f
TtB00 <88588878+TtB00@users.noreply.github.com>
2024-03-15 04:25:20 +0700

    update checkout

--- Front-Secondo/src/container/CheckOut/index.jsx
+++ Front-Secondo/src/container/CheckOut/index.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./index.css";
 import Layout from "../../components/Layout";
+import { NavLink, Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+
+
 
 function checkOut() {
+  // const addressOption = [
+  //   {
+  //     value: "1",
+  //     label: "บ้านคเณศ",
+  //   },
+  //   {
+  //     value: "2",
+  //     label: "บ้านพุฒิพงศ์",
+  //   },
+  //   {
+  //     value: "3",
+  //     label: "บ้านฐิติพงศ์",
+  //   },
+  // ];
+  
+  // const deliveryOption = [
+  //   {
+  //     value: "1",
+  //     label: "ส่งแบบมาตรฐาน",
+  //   },
+  //   {
+  //     value: "2",
+  //     label: "ส่งด่วน",
+  //   },
+  // ];
+  
+  // const paymentOption = [
+  //   {
+  //     value: "1",
+  //     label: "Master Card ending 1123",
+  //   },
+  //   {
+  //     value: "2",
+  //     label: "Master Card ending 3968",
+  //   },
+  // ];
+  // const [value, setValue] = useState<string | null>(null);
+
   return (
     <Layout>
       <div className="background-checkout-page">
@@ -24,13 +66,17 @@ function checkOut() {
                       ข้อมูลการจัดส่ง
                     </div>
                     <div className="checkout-page-content-method-address-group-detail-group-desc">
-                      บ้านฉัน
+                      ที่อยู่การจัดส่ง
                     </div>
                   </div>
                 </div>
                 <div className="checkout-page-content-method-address-group-edit">
-                  <div className="checkout-page-content-method-address-group-edit-text">
-                    แก้ไข
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to="/setting"
+                    className="checkout-page-content-method-address-group-edit-text"
+                  >
+                    กรุณาเพิ่มที่อยู่การจัดส่ง
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
@@ -46,7 +92,7 @@ function checkOut() {
                         stroke-linejoin="round"
                       />
                     </svg>
-                  </div>
+                  </Link>
                 </div>
               </div>
               <div className="checkout-page-content-method-address-line-frame">
@@ -70,7 +116,11 @@ function checkOut() {
                   </div>
                 </div>
                 <div className="checkout-page-content-method-address-group-edit">
-                  <div className="checkout-page-content-method-address-group-edit-text">
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to=""
+                    className="checkout-page-content-method-address-group-edit-text"
+                  >
                     แก้ไข
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -87,8 +137,69 @@ function checkOut() {
                         stroke-linejoin="round"
                       />
                     </svg>
+                  </Link>
+                </div>
+              </div>
+              <div className="checkout-page-content-method-address-line-frame">
+                <div className="checkout-page-content-method-address-line"></div>
+              </div>
+            </div>
+
+            <div className="checkout-page-content-method-address">
+              <div className="checkout-page-content-method-address-group">
+                <div className="checkout-page-content-method-address-group-detail">
+                  <div className="checkout-page-content-method-address-group-detail-number">
+                    2
+                  </div>
+                  <div className="checkout-page-content-method-address-group-detail-group">
+                    <div className="checkout-page-content-method-address-group-detail-group-topic">
+                      วิธีการจัดส่ง
+                    </div>
                   </div>
                 </div>
+                <div className="checkout-page-content-method-address-group-edit">
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to=""
+                    className="checkout-page-content-method-address-group-edit-text"
+                  >
+                    แก้ไข
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M4 6.00098L6.58579 8.58676C7.36684 9.36781 8.63317 9.36781 9.41421 8.58676L12 6.00098"
+                        stroke="#665C29"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+              <div className="checkout-page-content-method-address-option">
+                <div className="checkout-page-content-method-address-option-header">
+                  ที่อยู่จัดส่ง
+                </div>
+                <Form className="checkout-page-content-method-address-option-form">
+                  {["radio"].map((type) => (
+                    <div
+                      key={`default-${type}`}
+                      className="checkout-page-content-method-address-option-form-radio"
+                    >
+                      <Form.Check
+                        type={type}
+                        id={`default-${type}`}
+                        label={`default ${type}`}
+                      />
+                    </div>
+                  ))}
+                </Form>
               </div>
               <div className="checkout-page-content-method-address-line-frame">
                 <div className="checkout-page-content-method-address-line"></div>
@@ -111,7 +222,11 @@ function checkOut() {
                   </div>
                 </div>
                 <div className="checkout-page-content-method-address-group-edit">
-                  <div className="checkout-page-content-method-address-group-edit-text">
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to=""
+                    className="checkout-page-content-method-address-group-edit-text"
+                  >
                     แก้ไข
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -128,7 +243,7 @@ function checkOut() {
                         stroke-linejoin="round"
                       />
                     </svg>
-                  </div>
+                  </Link>
                 </div>
               </div>
               <div className="checkout-page-content-method-address-line-frame">
@@ -167,16 +282,28 @@ function checkOut() {
             </div>
             <div className="checkout-page-content-order-price">
               <div className="checkout-page-content-order-price-item">
-                <div className="checkout-page-content-order-price-item-topic kanit-paragraphSmall">ค่าสินค้า</div>
-                <div className="checkout-page-content-order-price-item-price kanit-paragraphSmall">129 บาท</div>
+                <div className="checkout-page-content-order-price-item-topic kanit-paragraphSmall">
+                  ค่าสินค้า
+                </div>
+                <div className="checkout-page-content-order-price-item-price kanit-paragraphSmall">
+                  129 บาท
+                </div>
               </div>
               <div className="checkout-page-content-order-price-deliver">
-                <div className="checkout-page-content-order-price-deliver-topic kanit-paragraphSmall">ค่าขนส่ง</div>
-                <div className="checkout-page-content-order-price-deliver-price kanit-paragraphSmall">50 บาท</div>
+                <div className="checkout-page-content-order-price-deliver-topic kanit-paragraphSmall">
+                  ค่าขนส่ง
+                </div>
+                <div className="checkout-page-content-order-price-deliver-price kanit-paragraphSmall">
+                  50 บาท
+                </div>
               </div>
               <div className="checkout-page-content-order-price-total">
-                <div className="checkout-page-content-order-price-total-topic kanit-Display-Small">รวม</div>
-                <div className="checkout-page-content-order-price-total-price kanit-Display-Small">179 บาท</div>
+                <div className="checkout-page-content-order-price-total-topic kanit-Display-Small">
+                  รวม
+                </div>
+                <div className="checkout-page-content-order-price-total-price kanit-Display-Small">
+                  179 บาท
+                </div>
               </div>
             </div>
             <button className="btn-small-primary kanit-paragraphMedium">
