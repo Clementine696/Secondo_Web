450c13752ba6498d1128e769e5f72edb29e74e5c
Cℓementine♥ <kanate--@hotmail.com>
2024-03-27 21:11:20 +0700

    Update Backend

--- mern-back-end/src/models/user.js
+++ mern-back-end/src/models/user.js
@@ -45,10 +45,10 @@ const userSchema = new mongoose.Schema({
     
 }, { timestamps: true });
 
-userSchema.virtual('password')
-.set(function(password){
-    this.hash_password = bcrypt.hashSync(password, 10);
-});
+// userSchema.virtual('password')
+// .set(function(password){
+//     this.hash_password = bcrypt.hashSync(password, 10);
+// });
 
 userSchema.virtual('fullname')
 .get(function(){
@@ -56,8 +56,9 @@ userSchema.virtual('fullname')
 });
 
 userSchema.methods = {
-    authenticate: function(password){
-        return bcrypt.compareSync(password, this.hash_password);
+    authenticate: async function(password){
+        // return bcrypt.compareSync(password, this.hash_password);
+        return await bcrypt.compare(password, this.hash_password);
     }
 }
 
--- mern-back-end/src/routes/product.js
+++ mern-back-end/src/routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct } = require('../controllers/product');
+const { createProduct, getProductsBySlug } = require('../controllers/product');
 const multer = require('multer');
 const router = express.Router();
 const shortid = require('shortid')
@@ -11,6 +11,8 @@ const storage = multer.diskStorage({
         cb(null, path.join(path.dirname(__dirname), 'uploads'))
     },
     filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
         cb(null, shortid.generate() + '-' + file.originalname)
     }
 })
@@ -18,6 +20,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
+router.get('/products/:slug', getProductsBySlug)
 // router.get('/category/getcategory', getCategory);
 
 module.exports = router;
