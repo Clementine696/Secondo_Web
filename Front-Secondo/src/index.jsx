c7a37453bf5db2d7e8f39222cd3962025a00415d
Cℓementine♥ <kanate--@hotmail.com>
2024-03-30 22:57:48 +0700

    Create Store

--- Front-Secondo/src/index.jsx
+++ Front-Secondo/src/index.jsx
@@ -1,36 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import { thunk } from 'redux-thunk';
-import rootReducer from "../reducers";
-import { configureStore, Tuple } from '@reduxjs/toolkit'
-
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: () => new Tuple(thunk),
-})
-
-window.store = store;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <Router>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Router>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
