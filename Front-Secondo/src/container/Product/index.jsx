76f741fc2a9d23894c05ba3b9144a5267ebb9625
TtB00 <88588878+TtB00@users.noreply.github.com>
2024-04-19 00:42:32 +0700

    update pathway donate

--- Front-Secondo/src/container/Product/index.jsx
+++ Front-Secondo/src/container/Product/index.jsx
@@ -44,66 +44,9 @@ function Product() {
     }
   };
 
-  // const [orderPathway, setOrderPathway] = useState(1);
-
-  // const clickPathway = (index) => {
-  //   setOrderPathway(index);
-  // };
-
   return (
     <Layout>
       <div className="background-product-page">
-        {/* <div className="product-page-path-way">
-          <div className="product-page-group-path-way">
-            <div className="product-page-group-path-way-before-path">
-              <div className="product-page-group-path-way-before-path-text kanit-paragraphtextMedium">
-                หน้าหลัก
-              </div>
-              <div className="product-page-group-path-way-before-path-arrow">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M9 18.001L13.5858 13.4152C14.3668 12.6341 14.3668 11.3678 13.5858 10.5868L9 6.00098"
-                    stroke="#00243D"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className="product-page-group-path-way-before-path">
-              <div className="product-page-group-path-way-before-path-text kanit-paragraphtextMedium">
-                เสื้อผ้า
-              </div>
-              <div className="product-page-group-path-way-before-path-arrow">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M9 18.001L13.5858 13.4152C14.3668 12.6341 14.3668 11.3678 13.5858 10.5868L9 6.00098"
-                    stroke="#00243D"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className="product-page-group-path-way-last-path kanit-paragraphtextMedium">
-              เสื้อ COTTON แบรนด์ญี่ปุ่น
-            </div>
-          </div>
-        </div> */}
         <div className="background-product-page-pathway">
           <Pathway pathwayList={pathway} />
         </div>
